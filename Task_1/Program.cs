bfae9a88ec26ea569c974e0c420d6f647dd42ac2
Zhurbaa <sana22_zhto@student.ztu.edu.ua>
2022-11-11 18:17:09 +0200

    Другій коміт текст

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("Український кабанчик");
 Console.WriteLine("Гітхаб");
 Console.WriteLine("Курси");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Слава Україні");
+Console.WriteLine("Тортік");
